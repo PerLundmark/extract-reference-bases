c96fff67a8993e6c2f9af97afbb368353beffa38
PerLundmark <per.lundmark@medsci.uu.se>
2016-10-13 18:28:55 +0200

    Implemented manifest parsing for Infinium

--- extract_reference_bases.py
+++ extract_reference_bases.py
@@ -3,20 +3,25 @@
 A script that extracts reference genome bases for
 genotyped positions found in the manifest of a genotyping chip
 or similar.
+
 """
 import argparse
 import ref_base_utils
 from datetime import date
+
+assembly = "b37"
 
 parser = argparse.ArgumentParser(description="Generate a reference base information file for Chiasma VCF exports (Illumina manifest and Agena/general format supported)")
 parser.add_argument('-f', '--format', required=True, choices=['infinium', 'agena'], help="Format of input manifest, either 'illumina' for Illumina Infinium, or 'agena' for a mart export for Agena/general use.")
 parser.add_argument('-i', '--input', required=True, help="Input filename.")
 parser.add_argument('-o', '--output', required=True, help= "Output filename.")
 parser.add_argument('-g', '--genome', required=True, help= "The source genome for reference base extraction. Fasta format.")
-args = parser.parse_args()
+parser.add_argument('-q', '--quiet', help= "Supress progress printing")
 
+args = parser.parse_args()
 current_date = date.today()
 
+#Build VCF header
 header_lines = []
 header_lines.append("##fileformat=VCFv4.1")
 header_lines.append("fileDate={:%Y%m%d}".format(current_date))
@@ -24,7 +29,28 @@ header_lines.append("##source=extract_reference_bases.pl_and_chiasma")
 header_lines.append("##reference=file://" + args.genome)
 header_lines.append("##phasing=none")
 
-references = ref_base_utils.build_seq_dict(args.genome)
-ref_keys = references.keys()
+(seq_info_list, seq_dict) = ref_base_utils.build_seq_dict(args.genome)
+
+for (current_id, current_length) in seq_info_list:
+    header_lines.append('##contig=<ID=' + current_id + ',length=' + str(current_length) + ',assembly=' + assembly + '>')
+
+header_lines.append("##FORMAT=<ID=GT,Number=1,Type=Integer,Description='Genotype'>")
+
+#Read manifest
+if(args.format == 'infinium'):
+    manifest = ref_base_utils.InfiniumManifest(args.input)
+elif(args.format == 'agena'):
+    manifest = ref_base_utils.AgenaManifest(args.input)
+else:
+    raise ValueError("Unknown manifest format")
+
+manifest.read_manifest()
+manifest.set_chr_order([row[0] for row in seq_info_list])   #[row[i] for i in manifest_indices]
+manifest.sort_markerlist()
+markers = manifest.get_markerlist()
+
+#Retrieve reference data
+manifest = None
 
 
+print("Hepp")
--- ref_base_utils.py
+++ ref_base_utils.py
@@ -1,8 +1,10 @@
 """
-Sequence / biopython utility functions
+Sequence / biopython utility classes and functions
 """
 
 from Bio import SeqIO
+from abc import ABC, abstractmethod
+import csv
 
 def build_seq_dict(fasta_filename):
     """Builds a dict of Biopython SeqRecord objects from a fasta file (kept in memory)
@@ -11,10 +13,18 @@ def build_seq_dict(fasta_filename):
         fasta_filename (str): The file name of the (multi sequence) fasta file to parse.
 
     Returns:
-        A dict of SeqRecord objects
+        A list of tuples with (id, name, length) of sequence records, and a dict of SeqRecord objects
     """
-    return SeqIO.to_dict(SeqIO.parse(fasta_filename, "fasta"))
+    records = list(SeqIO.parse(fasta_filename, "fasta"))
+    records_info = []
+    records_dict = {}
 
+    for record in records:
+        info_record = (record.id, len(record))
+        records_info.append(info_record)
+        records_dict[record.id] = record
+
+    return (records_info, records_dict)
 
 def build_indexed_seq_dict(fasta_filename):
     """Builds a dict-like object of Biopython SeqRecord objects from a fasta file (indexed on disk)
@@ -26,3 +36,81 @@ def build_indexed_seq_dict(fasta_filename):
         A dict of indexed SeqRecord objects
     """
     return SeqIO.index(fasta_filename, "fasta")
+
+class GenotypingManifest(ABC):
+    """Abstract base class for genotyping manifests"""
+    def __init__(self, filename):
+        self.filename = filename
+        self.marker_list = []
+        self.chr_order = []
+
+    @abstractmethod
+    def read_manifest(self, filename):
+        pass
+
+    @abstractmethod
+    def get_markerlist(self):
+        pass
+
+    @abstractmethod
+    def set_chr_order(self, chr_list):
+        pass
+
+    @abstractmethod
+    def sort_markerlist(self):
+        pass
+
+class InfiniumManifest(GenotypingManifest):
+    """A Class for Illumina Infinium manifests (csv-format)"""
+    def read_manifest(self):
+        try:
+            with open(self.filename, newline='') as infinium_manifest:
+                reader = csv.reader(infinium_manifest, delimiter=',')
+                while (next(reader)[0] != "[Assay]"): # Skip header
+                    pass
+
+                header = next(reader) #Check positions of fields in manifest (variable order)
+                idx_snp_name = header.index('Name')
+                idx_snp_chr = header.index('Chr')
+                idx_snp_pos = header.index('MapInfo')
+                idx_snp_var = header.index('SNP')
+                idx_snp_ref_strand = header.index('RefStrand')
+
+                manifest_indices = [idx_snp_name, idx_snp_chr, idx_snp_pos, idx_snp_var, idx_snp_ref_strand]
+                self.marker_list = []
+                for row in reader:
+                    if(row[0] == "[Controls]"): #End of SNP data
+                        break
+                    self.marker_list.append([row[i] for i in manifest_indices])
+
+        except IOError:
+            print("Could not read file: ", self.filename)
+
+
+    def get_markerlist(self):
+        return self.marker_list
+
+    def set_chr_order(self, chr_list):
+        self.chr_order = chr_list
+
+    def get_sort_keys(self, item):
+        try:
+            chr_sort_key = self.chr_order.index(item(1))
+        except ValueError:
+            chr_sort_key = len(self.chr_order) #Sort 0/unknown/strange chromosome entries behind the rest
+
+        return (chr_sort_key, item[2], item[0])
+
+    def sort_markerlist(self):
+#        import functools
+#        key = functools.partial(self.get_sort_key, data=something)
+#        self.marker_list.sort(key=key)
+        self.marker_list.sort(key=self.get_sort_keys)
+
+
+class AgenaManifest(GenotypingManifest):
+    def read_manifest(self, filename):
+        pass
+
+    def get_markerlist(self):
+        pass
